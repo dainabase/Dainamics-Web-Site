39bd277d68ceb5452dac3e4a04dd9d7217e58f8f
dainabase <admin@dainamics.ch>
2025-11-26 09:49:59 +0200

    Added index.ts

---
+++ src/components/problemes/index.ts
@@ -0,0 +1,7 @@
+export { ProblemeHero } from './ProblemeHero';
+export { ProblemePointsDouleur } from './ProblemePointsDouleur';
+export { ProblemeSolution } from './ProblemeSolution';
+export { ProblemeCasClient } from './ProblemeCasClient';
+export { ProblemeProcessus } from './ProblemeProcessus';
+export { ProblemeInvestissement } from './ProblemeInvestissement';
+export { ProblemeCTA } from './ProblemeCTA';
